bf63bc77c32313fd0782df0b3493bbb259b1a1e5
yilmaz13 <ahhmet13yilmaz@gmail.com>
2024-12-03 23:12:49 +0300

    Added Slider for Armor and Hp System

--- Assets/Scripts/Resources/ResourceReferences.cs
+++ Assets/Scripts/Resources/ResourceReferences.cs
@@ -3,16 +3,18 @@ using UnityEngine;
 public class ResourceReferences : MonoBehaviour
 {
     //  MEMBERS      
-    public GameObject GameUIPrefab { get { return _gameUIPrefab; } }
-    public GameObject LoadingUIView { get { return _loadingUIView; } }
-    public GameObject GameViewPrefab { get { return _gameViewPrefab; } }
-
+    public GameObject GameUIPrefab     { get { return _gameUIPrefab; } }
+    public GameObject LoadingUIView    { get { return _loadingUIView; } }
+    public GameObject GameViewPrefab   { get { return _gameViewPrefab; } }
     public GameObject MainMenuUIPrefab { get { return _mainMenuUIPrefab; } }
     public GameResources GameResources { get { return _gameResources; } }
+    public GameObject ProjectilePoolPrefab { get { return _projectilePoolPrefab; } }
+   
 
-    [SerializeField] private GameObject _gameUIPrefab;
-    [SerializeField] private GameObject _loadingUIView;
-    [SerializeField] private GameObject _gameViewPrefab;
-    [SerializeField] private GameObject _mainMenuUIPrefab;
+    [SerializeField] private GameObject    _gameUIPrefab;
+    [SerializeField] private GameObject    _loadingUIView;
+    [SerializeField] private GameObject    _gameViewPrefab;
+    [SerializeField] private GameObject    _mainMenuUIPrefab;
+    [SerializeField] private GameObject    _projectilePoolPrefab;
     [SerializeField] private GameResources _gameResources;
 }
