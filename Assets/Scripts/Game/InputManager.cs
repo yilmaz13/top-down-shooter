71316345128889531b5375df7a67b828c032e143
yilmaz13 <ahhmet13yilmaz@gmail.com>
2024-12-10 16:59:00 +0300

    Updated Input Manager

--- Assets/Scripts/Game/InputManager.cs
+++ Assets/Scripts/Game/InputManager.cs
@@ -1,6 +1,55 @@
+using System;
 using UnityEngine;
 
-public class InputManager
+public class InputManager : MonoBehaviour
 {
-   
+    private Vector2 _inputVector;
+    private Vector2 _mousePosition;
+    public static InputManager Instance { get; private set; }
+
+    public Vector2 InputVector   => _inputVector;
+    public Vector3 MousePosition => _mousePosition;
+
+    public event Action OnFire;
+    public event Action<int> OnSwitchWeapon;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Update()
+    {
+        var horizontal = Input.GetAxis("Horizontal");
+        var vertical   = Input.GetAxis("Vertical");
+
+        _inputVector   = new Vector2(horizontal, vertical);
+        _mousePosition = Input.mousePosition;
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            OnFire?.Invoke();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            OnSwitchWeapon?.Invoke(0);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            OnSwitchWeapon?.Invoke(1);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            OnSwitchWeapon?.Invoke(2);
+        }
+    }
 }
